736bc0517cd98e641cfe38349554c2f1a70dec51
Morshedkhondokar <morshedkhondokar99@gmail.com>
2025-06-28 00:28:33 +0600

    mobile screen problem fixed

--- src/Components/AvailableBtns/AvailableBtn.jsx
+++ src/Components/AvailableBtns/AvailableBtn.jsx
@@ -5,13 +5,13 @@ const AvailableBtn = ({SelectedActiveBtn, handleSelectedActiveBtn}) => {
   return (
     <div className="max-w-[1280px] mx-auto sticky top-[71px] rounded-xl bg-white/10 backdrop-blur-lg border-b border-white/20 h-12">
         <div className="flex justify-between items-center mt-10 md:mt-20 mb-8 ">
-             <h1 className="text-[19px] md:text-[20px] font-bold">
+             <h1 className="text-[16px] pl-1 md:text-[20px] font-semibold">
               {SelectedActiveBtn === "btn1" ? "Available Players" : "Selected Player (4/6)"}
               </h1>
       <div>
         <button
           onClick={() => handleSelectedActiveBtn("btn1")}
-          className={`border-r-0 p-2 md:py-3 md:px-5 bg-[#e7e7e7]  border rounded-l-xl  ${
+          className={`border-r-0 p-1 md:py-3 md:px-5 bg-[#e7e7e7]  border rounded-l-xl  ${
             SelectedActiveBtn === "btn1"
               ? "bg-[#e7fe29] border-[#d2e727] font-semibold"
               : " border-[#e7e7e7] "
@@ -21,7 +21,7 @@ const AvailableBtn = ({SelectedActiveBtn, handleSelectedActiveBtn}) => {
         </button>
         <button
           onClick={() => handleSelectedActiveBtn("btn2")}
-          className={`border-r-0 p-2 md:py-3 md:px-5 bg-[#e7e7e7]  border rounded-r-xl
+          className={`border-r-0 p-1 md:py-3 md:px-5 bg-[#e7e7e7]  border rounded-r-xl
              ${SelectedActiveBtn === "btn2"
               ? "bg-[#e7fe29] border-[#d2e727] font-semibold"
               : " border-[#e7e7e7] " } `}
