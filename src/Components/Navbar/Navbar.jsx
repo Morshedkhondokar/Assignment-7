6b0cba82214072705c00ddf43eac2af76805d448
Morshedkhondokar <morshedkhondokar99@gmail.com>
2025-06-28 00:14:41 +0600

    image problem done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = ({coinAdd}) => {
         bg-white/10 backdrop-blur-lg 
         sticky top-0 px-6 z-10 ">
             <div>
-                <img src="../../../public/images/logo.png" alt="" />
+                <img src="images/logo.png" alt="" />
             </div>
             <div className="flex gap-6 justify-center items-center">
                 <ul className="flex gap-5 text-[#a1a1a1] font-bold hidden md:flex">
@@ -17,7 +17,7 @@ const Navbar = ({coinAdd}) => {
                 <div className="flex justify-center items-center gap-2 border border-[#e7e7e7] p-2 rounded-xl
                 font-semibold">
                     {coinAdd} Coin
-                    <img className="w-5 h-5" src="../../../public/images/coin.png" alt="" />
+                    <img className="w-5 h-5" src="images/coin.png" alt="" />
                 </div>
             </div>
         </div>
